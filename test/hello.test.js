68dc1cb8ee8b8ab2bb08187d918cb10678ea568b
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-05-02 15:25:41 -0700

    swap convert to composite in test

--- test/hello.test.js
+++ test/hello.test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var module = require('../lib/index.js');
 
 test('prints world', function(t) {
-  var check = module.convert();
+  var check = module.composite();
   t.equal(check, 'hello world', 'returned world');
   t.end();
 });
