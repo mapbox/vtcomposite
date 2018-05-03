2961f61b0c0aa487e57f9a0c87723a74c545db65
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-05-02 17:16:39 -0700

    fix formatting

--- src/module.cpp
+++ src/module.cpp
@@ -2,14 +2,14 @@
 // #include "your_code.hpp"
 // artem imports his library here
 
-namespace standalone{
-  NAN_METHOD(composite) {
-      // "info" comes from the NAN_METHOD macro, which returns differently
-      // according to the version of node
-      info.GetReturnValue().Set(
-          Nan::New<v8::String>("hello world").ToLocalChecked());
-  }
+namespace standalone {
+NAN_METHOD(composite) {
+    // "info" comes from the NAN_METHOD macro, which returns differently
+    // according to the version of node
+    info.GetReturnValue().Set(
+        Nan::New<v8::String>("hello world").ToLocalChecked());
 }
+} // namespace standalone
 // "target" is a magic var that nodejs passes into a module's scope.
 // When you write things to target, they become available to call from
 // Javascript world.
@@ -17,7 +17,6 @@ static void init(v8::Local<v8::Object> target) {
 
     // expose hello method
     Nan::SetMethod(target, "composite", standalone::composite);
-
 }
 
 NODE_MODULE(module, init) // NOLINT
