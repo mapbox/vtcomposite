ea3984914e1aa433fa37ea4e92d453642d24a2ef
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-05-03 17:07:16 -0700

    api signature draft test

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,9 +1,21 @@
 var test = require('tape');
 var module = require('../lib/index.js');
 
-test('prints world', function(t) {
-  var check = module.composite();
-  t.equal(check, 'hello world', 'returned world');
+
+test('[composite] composites two tiles at same zoom level successfully', function(t) {
+  var vtileSourceBuffers = [
+    {buffer: Buffer(), z:3, x:1, y:0}, 
+    {buffer: Buffer(), z:3, x:1, y:1}
+  ];
+  
+  var zxy_ofmaprequest = {z:3, x:0, y:1}; 
+
+  var options = {}; 
+
+  module.composite(vtileSourceBuffers, zxy_ofmaprequest, options, function(err, vtBuffer){
+    // console.log(vtBuffer); 
+    // t.equal(vtBuffer, 'mocked output buffer', 'returned single buffer');
+  });
   t.end();
 });
 
