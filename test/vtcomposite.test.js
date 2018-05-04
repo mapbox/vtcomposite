60c402303ec2c8fa24289e0374bc1558e35c8446
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-05-04 16:57:56 -0700

    add async function

--- test/vtcomposite.test.js
+++ test/vtcomposite.test.js
@@ -1,7 +1,37 @@
-'use strict'
-
 var test = require('tape');
 var module = require('../lib/index.js');
+var fs = require('fs');
+var path = require('path');
+var bufferSF = fs.readFileSync(path.resolve(__dirname+'/../node_modules/@mapbox/mvt-fixtures/real-world/sanfrancisco/15-5238-12666.mvt'));
+
+
+test.only('[composite] composites successfully', function(t) {
+
+  const buffer = bufferSF;
+
+  var vtileSourceBuffers = [
+    {buffer: buffer, z:3, x:1, y:0}
+  ];
+  
+  var zxy_ofmaprequest = {z:3, x:0, y:1}; 
+
+  var options = {}; 
+
+  module.composite(vtileSourceBuffers, zxy_ofmaprequest, options, function(err, vtBuffer){
+    // console.log('');
+    console.log('vtbuffer', vtBuffer);
+    // vtBuffer.features.forEach(function(feature) {
+    //   console.log('FEATURE', feature);
+    //   // assert.ok(feature.properties.tilequery.distance <= 1000, 'less than radius');
+    // });
+    console.log('errror', err.message); 
+    t.end();
+    // t.equal(vtBuffer, 'mocked output buffer', 'returned single buffer');
+  });
+  // t.end();
+});
+
+// vtzero has class vector tile - accepts a pointer to a buffer and the actual length of it. 
 
 test('failure: fails without callback function', assert => {
   try {
@@ -11,3 +41,371 @@ test('failure: fails without callback function', assert => {
     assert.end();
   }
 });
+
+// TESTS FOR BUFFER OBJECT! 
+
+test('failure: buffers is not an array', assert => {
+  module.composite('i am not an array', {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'first arg \'tiles\' must be an array of tile objects');
+    assert.end();
+  });
+});
+
+test('failure: buffers array is empty', assert => {
+  const buffs = [];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'tiles\' array must be of length greater than 0');
+    assert.end();
+  });
+});
+
+test('failure: item in buffers array is not an object', assert => {
+  const buffs = [
+    'not an object'
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'items in \'tiles\' array must be objects');
+    assert.end();
+  });
+});
+
+test('failure: buffer value does not exist', assert => {
+  const buffs = [
+    {
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a buffer value');
+    assert.end();
+  });
+});
+
+test('failure: buffer value is null', assert => {
+  const buffs = [
+    {
+      buffer: null,
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'buffer value in \'tiles\' array item is null or undefined');
+    assert.end();
+  });
+});
+
+test('failure: buffer value is not a buffer', assert => {
+  const buffs = [
+    {
+      buffer: 'not a buffer',
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'buffer value in \'tiles\' array item is not a true buffer');
+    assert.end();
+  });
+});
+
+test('failure: buffer object missing z value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      // z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'z\' value');
+    assert.end();
+  });
+});
+
+test('failure: buffer object missing x value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      // x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'x\' value');
+    assert.end();
+  });
+});
+
+test('failure: buffer object missing y value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      // y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'y\' value');
+    assert.end();
+  });
+});
+
+test('failure: buffer object z value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 'zero',
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'z\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: buffer object x value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 'zero',
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'x\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: buffer object y value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 'zero'
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'y\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: buffer object z value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: -10,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'z\' value must not be less than zero');
+    assert.end();
+  });
+});
+
+test('failure: buffer object x value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: -5,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'x\' value must not be less than zero');
+    assert.end();
+  });
+});
+
+test('failure: buffer object y value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: -4
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'y\' value must not be less than zero');
+    assert.end();
+  });
+});
+
+// TESTS FOR ZXY MAP REQUEST! 
+
+test('failure: map request zxy missing z value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'z\' value');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy missing x value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      // x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'x\' value');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy missing y value', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      // y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, 'item in \'tiles\' array does not include a \'y\' value');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy z value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:'zero', x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'z\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy x value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:'zero', y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'x\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy y value is not a number', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:'zero'}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'y\' value in \'tiles\' array item is not a number');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy z value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 10,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:-3, x:1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'z\' value must not be less than zero');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy x value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:-1, y:0}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'x\' value must not be less than zero');
+    assert.end();
+  });
+});
+
+test('failure: map request zxy y value is negative', assert => {
+  const buffs = [
+    {
+      buffer: new Buffer('hey'),
+      z: 0,
+      x: 0,
+      y: 0
+    }
+  ];
+  module.composite(buffs, {z:3, x:1, y:-4}, {}, function(err, result) {
+    assert.ok(err);
+    assert.equal(err.message, '\'y\' value must not be less than zero');
+    assert.end();
+  });
+});
+
