d42ade72fe180de1577d1ee596c62b9f8f9bd05c
Ann Millspaugh <ann.millspaugh@gmail.com>
2018-05-16 12:10:09 -0700

    add benching scenarios

--- bench/vtcomposite.bench.js
+++ bench/vtcomposite.bench.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2));
 if (!argv.iterations || !argv.concurrency) {
   console.error('Please provide desired iterations, concurrency');
-  console.error('Example: \n\tnode bench/vtcomposite.bench.js --iterations 50 --concurrency 10');
-  console.error('Optional args: \n\t--mem (reports memory stats)');
+  console.error('Example: \nnode bench/vtquery.bench.js --iterations 50 --concurrency 10');
   process.exit(1);
 }
 
@@ -14,82 +13,78 @@ if (!argv.iterations || !argv.concurrency) {
 // top of a JS file (like we do here)
 process.env.UV_THREADPOOL_SIZE = argv.concurrency;
 
-var fs = require('fs');
-var path = require('path');
-var bytes = require('bytes');
-var assert = require('assert')
-var d3_queue = require('d3-queue');
-var composite = require('../lib/index.js');
-var queue = d3_queue.queue();
-var bufferSF = fs.readFileSync(path.resolve(__dirname+'/../node_modules/@mapbox/mvt-fixtures/real-world/sanfrancisco/15-5238-12666.mvt'));
-var bufferNepal = fs.readFileSync(path.resolve(__dirname+'/../node_modules/@mapbox/mvt-fixtures/real-world/nepal/13-6036-3426.mvt'));
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const Queue = require('d3-queue').queue;
+const vtquery = require('../lib/index.js');
+const rules = require('./rules');
+let ruleCount = 1;
 
-var tiles = [
-  {buffer: bufferSF, z:15, x:5238, y:12666},
-  {buffer: bufferNepal, z:15, x:5238, y:12666}
-];
+// run each rule synchronously
+const ruleQueue = Queue(1);
+rules.forEach(function(rule) {
+  ruleQueue.defer(runRule, rule);
+});
 
-var zxy = {z:15, x:5238, y:12666};
+ruleQueue.awaitAll(function(err, res) {
+  if (err) throw err;
+  process.stdout.write('\n');
+});
 
-var track_mem = argv.mem ? true : false;
-var runs = 0;
-var memstats = {
-  max_rss:0,
-  max_heap:0,
-  max_heap_total:0
-};
+function runRule(rule, ruleCallback) {
 
-function run(cb) {
-  // running our module
-  composite(tiles, zxy, {}, function(err, result) {
+  process.stdout.write(`\n${ruleCount}: ${rule.description} ... `);
+
+  let runs = 0;
+  let runsQueue = Queue();
+
+  function run(cb) {
+    vtquery(rule.tiles, rule.zxy, rule.options, function(err, result) {
       if (err) {
         return cb(err);
       }
       ++runs;
-      if (track_mem && runs % 1000) {
-        var mem = process.memoryUsage();
-        if (mem.rss > memstats.max_rss) memstats.max_rss = mem.rss;
-        if (mem.heapTotal > memstats.max_heap_total) memstats.max_heap_total = mem.heapTotal;
-        if (mem.heapUsed > memstats.max_heap) memstats.max_heap = mem.heapUsed;
-      }
       return cb();
+    });
+  }
+
+  // Start monitoring time before async work begins within the defer iterator below.
+  // AsyncWorkers will kick off actual work before the defer iterator is finished,
+  // and we want to make sure we capture the time of the work of that initial cycle.
+  var time = +(new Date());
+
+  for (var i = 0; i < argv.iterations; i++) {
+    runsQueue.defer(run);
+  }
+
+  runsQueue.awaitAll(function(error) {
+    if (error) throw error;
+    if (runs != argv.iterations) {
+      throw new Error(`Error: did not run as expected - ${runs} != ${argv.iterations}`);
+    }
+    // check rate
+    time = +(new Date()) - time;
+
+    if (time == 0) {
+      console.log("Warning: ms timer not high enough resolution to reliably track rate. Try more iterations");
+    } else {
+    // number of milliseconds per iteration
+      var rate = runs/(time/1000);
+      process.stdout.write(rate.toFixed(0) + ' runs/s (' + time + 'ms)');
+    }
+
+    // There may be instances when you want to assert some performance metric
+    //assert.equal(rate > 1000, true, 'speed not at least 1000/second ( rate was ' + rate + ' runs/s )');
+    ++ruleCount;
+    return ruleCallback();
   });
 }
 
-// Start monitoring time before async work begins within the defer iterator below.
-// AsyncWorkers will kick off actual work before the defer iterator is finished,
-// and we want to make sure we capture the time of the work of that initial cycle.
-var time = +(new Date());
-
-for (var i = 0; i < argv.iterations; i++) {
-    queue.defer(run);
-}
-
-queue.awaitAll(function(error) {
-  if (error) throw error;
-  if (runs != argv.iterations) {
-    throw new Error('Error: did not run as expected');
-  }
-  // check rate
-  time = +(new Date()) - time;
-
-  if (time == 0) {
-    console.log('Warning: ms timer not high enough resolution to reliably track rate. Try more iterations');
+function log(message) {
+  if (argv.output && argv.output === 'json') {
+    // handle JSON output
   } else {
-  // number of milliseconds per iteration
-    var rate = runs/(time/1000);
-    console.log('Benchmark speed: ' + rate.toFixed(0) + ' runs/s (runs:' + runs + ' ms:' + time + ' )');
-
-    if (track_mem) {
-      console.log('Benchmark peak mem (max_rss, max_heap, max_heap_total): ', bytes(memstats.max_rss), bytes(memstats.max_heap), bytes(memstats.max_heap_total));
-    } else {
-      console.log('Note: pass --mem to track memory usage');
-    }
+    process.stdout.write(message);
   }
-
-  console.log('Benchmark iterations:',argv.iterations,'concurrency:',argv.concurrency)
-
-  // There may be instances when you want to assert some performance metric
-  //assert.equal(rate > 1000, true, 'speed not at least 1000/second ( rate was ' + rate + ' runs/s )');
-
-});
+}
