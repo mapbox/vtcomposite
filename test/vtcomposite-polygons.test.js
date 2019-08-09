fce6a9c5294f982d54c45226f4dd33b79ffd88ef
Artem Pavlenko <artem@mapnik.org>
2019-08-09 15:40:15 +0100

    add test (ref #100)

--- test/vtcomposite-polygons.test.js
+++ test/vtcomposite-polygons.test.js
@@ -129,3 +129,39 @@ test('[composite] composite and overzooming success polygons - overzooming polyg
     assert.end();
   });
 });
+
+test('[composite] composite and overzooming polygons with holes', function(assert) {
+  const zxy = { z:8, x:221, y:99 };
+  const parent = { z:4, x:13, y:6 };
+
+  const tiles = [
+    { buffer: fs.readFileSync('./test/fixtures/polygons-with-holes-4-13-6.mvt'),  z: parent.z, x: parent.x, y: parent.y }
+  ];
+
+  composite(tiles, zxy, {buffer_size:4080}, (err, vtBuffer) => {
+    assert.notOk(err);
+    assert.equal(vtBuffer.length, 848);
+    const outputInfo = vtinfo(vtBuffer);
+    // one feature
+    assert.equal(outputInfo.layers.polygons.feature.length, 1);
+    var feature = outputInfo.layers.polygons.feature(0);
+    var geojson = feature.toGeoJSON(zxy.x,zxy.y,zxy.z);
+    var coords = geojson.geometry.coordinates;
+    // two polygons
+    assert.equal(coords.length,2);
+    // first polygons 8 rings
+    assert.equal(coords[0].length, 8);
+    assert.equal(coords[0][0].length, 95);
+    assert.equal(coords[0][1].length, 25);
+    assert.equal(coords[0][2].length, 23);
+    assert.equal(coords[0][3].length, 18);
+    assert.equal(coords[0][4].length, 26);
+    assert.equal(coords[0][5].length, 18);
+    assert.equal(coords[0][6].length, 14);
+    assert.equal(coords[0][7].length, 16);
+    // second polygon 1 ring
+    assert.equal(coords[1].length,1);
+    assert.equal(coords[1][0].length, 6);
+    assert.end();
+  });
+});
