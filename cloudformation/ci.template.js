20d27ce2ce764d8079139dfca040c047ea33367f
Sam Matthews <matthews.sam@gmail.com>
2021-04-15 17:33:41 -0700

    remove delete permissions

--- cloudformation/ci.template.js
+++ cloudformation/ci.template.js
@@ -31,7 +31,7 @@ module.exports = {
             PolicyDocument: {
               Statement: [
                 {
-                  Action: ['s3:DeleteObject', 's3:GetObject', 's3:GetObjectAcl', 's3:PutObject', 's3:PutObjectAcl'],
+                  Action: ['s3:GetObject', 's3:GetObjectAcl', 's3:PutObject', 's3:PutObjectAcl'],
                   Effect: 'Allow',
                   Resource: 'arn:aws:s3:::mapbox-node-binary/' + package_json.name + '/*'
                 }
