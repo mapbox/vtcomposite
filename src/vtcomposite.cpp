bbc7de69da0d3a6fc65373bd40a78f2c968c6bdb
Kien Bui <kien.bui@mapbox.com>
2024-10-23 11:09:11 +0300

    fix localize bug

--- src/vtcomposite.cpp
+++ src/vtcomposite.cpp
@@ -989,7 +989,8 @@ struct LocalizeWorker : Napi::AsyncWorker
 
                             if (language_property_value.string_value() != original_language_value.string_value())
                             {
-                                final_properties.emplace_back(language_property_key, language_property_value);
+                                if(language_property_value.valid())
+                                    final_properties.emplace_back(language_property_key, language_property_value);
                             }
                         }
                     }
